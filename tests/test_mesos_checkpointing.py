29342ac60fb06f2ccb194258c35c7dd21999019f
farhan5900 <farhan5900@gmail.com>
2020-06-16 12:37:34 -0300

    Adds sleeps to avoid task name conflict

--- tests/test_mesos_checkpointing.py
+++ tests/test_mesos_checkpointing.py
@@ -6,9 +6,11 @@ import sdk_networks
 import sdk_tasks
 import shakedown
 import spark_utils as utils
+import time
 
 log = logging.getLogger(__name__)
 
+app_name = "MockTaskRunner"
 driver_cpus = None
 executor_cpus = None
 
@@ -45,7 +47,7 @@ def setup_spark(configure_security_spark, configure_universe):
 
 @pytest.mark.sanity
 def test_agent_restart_with_checkpointing_disabled():
-    (driver_task_id, driver_task, executor_task) = _submit_job_and_get_tasks(app_name="MockTaskRunner_CP_Disabled")
+    (driver_task_id, driver_task, executor_task) = _submit_job_and_get_tasks()
 
     driver_ip = sdk_networks.get_task_host(driver_task)
     executor_ip = sdk_networks.get_task_host(executor_task)
@@ -55,13 +57,12 @@ def test_agent_restart_with_checkpointing_disabled():
     utils.restart_task_agent_and_verify_state(executor_ip, executor_task, "TASK_LOST")
 
     _kill_driver_task(driver_task_id)
+    time.sleep(30)  # To avoid tasks conflict
 
 
 @pytest.mark.sanity
 def test_agent_restart_with_checkpointing_enabled():
-    (driver_task_id, driver_task, executor_task) = _submit_job_and_get_tasks(
-        app_name="MockTaskRunner_CP_Enabled",
-        extra_args=["--conf spark.mesos.checkpoint=true"])
+    (driver_task_id, driver_task, executor_task) = _submit_job_and_get_tasks(extra_args=["--conf spark.mesos.checkpoint=true"])
 
     driver_ip = sdk_networks.get_task_host(driver_task)
     executor_ip = sdk_networks.get_task_host(executor_task)
@@ -72,12 +73,11 @@ def test_agent_restart_with_checkpointing_enabled():
     _kill_driver_task(driver_task_id)
 
 
-def _submit_job_and_get_tasks(app_name, extra_args=[]):
+def _submit_job_and_get_tasks(extra_args=[]):
     submit_args = ["--conf spark.driver.cores={}".format(driver_cpus),
                    "--conf spark.cores.max={}".format(executor_cpus),
                    "--conf spark.executor.cores={}".format(executor_cpus),
-                   "--conf spark.app.name={}".format(app_name),
-                   "--class MockTaskRunner"
+                   "--class {}".format(app_name)
                    ] + extra_args
 
     driver_task_id = utils.submit_job(app_url=utils.dcos_test_jar_url(),
